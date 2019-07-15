537e4817444b4505c8d35ec9ba506c929c455b68
YancaHernandez <52180659+YancaHernandez@users.noreply.github.com>
2019-07-15 09:38:35 -0500

    Update votantes.r

--- votantesAlgoritmo/votantes.r
+++ votantesAlgoritmo/votantes.r
@@ -1,12 +1,5 @@
-
-candidatos<-data.frame("blanco"=0)
-can<-"Yan";
-#candidatos<-c(votos,"yesid"=10)
-candidatos<-c(candidatos,textConnection(can));
-print(candidatos)
-
-
-registrarCandidato<-function(candidatos){
+3### OPCION 1 DEL MENU
+registrarCandidato<-function(candidatos,votos){
     limite<-3;
     if(length(candidatos)>=limite){
         print(paste("Solo pueden registrar ", limite, " candidatos"));
@@ -14,33 +7,53 @@ registrarCandidato<-function(candidatos){
         return(candidatos);
     }else{
         can<-readline("Nombre del candidato: ");
-        candidatos<-c(candidatos,cant(can)=0);
-        #votos<-c(votos,print(can)=0);        
-        return (candidatos);
+        candidatos<-c(candidatos,can);
+        votos<-c(votos,0);
+        assign("candidatos", candidatos, envir = .GlobalEnv)
+        assign("votos", votos, envir = .GlobalEnv)
+        return (0);
     }
 }
 
-
-
+#OPCION 3 DEL MENU
 listarCandidatos<-function(candidatos){
     print("LISTA DE CANDIDATOS")
     for(can in candidatos){
         print(can);
     }
-    opt<-readline("Elija un candidato: ");
-    
-    
     flush.console();
+    return(0);
 }
 
-votar<-function(candidatos){
+
+#OPCION 4 DEL MENU
+votar<-function(candidatos,votos){
+    print("LISTA DE CANDIDATOS")
     for(i in 1:length(candidatos)){
         print(paste(i," - ",candidatos[i]));
     }
+    flush.console();
+    opt<-readline("Elija su candidato: ");
+    opt<-as.numeric(opt);
+    
+    votos[opt]<-votos[opt]+1;
+    assign("votos", votos, envir = .GlobalEnv)
+    votoAzar(votos);
+    return(0);
 }
 
-candidatos<-data.frame("blanco"=0)
-votos<-c(blanco=0);
+votoAzar<-function(votos){
+    num<-length(votos)
+    v<-sample(1:num,1);
+    votos[v]<-votos[v]+1;
+    assign("votos", votos, envir = .GlobalEnv)
+    return(0);
+}
+
+
+candidatos<-c("blanco","Yesid")
+votos<-c(0,0);
+
 print("MENU DE OPCIONES");
 print("1 Registrar candidato");
 print("2 Reiniciar votos");
@@ -52,17 +65,20 @@ print("-1 Salir");
 while(1){
     opt<-readline("Elija una opcion: ");
     if(opt==1){
-        candidatos<-registrarCandidato(candidatos);
-        #candidatos<-resul[1];
-        print(candidatos);
-        flush.console();
-        #votos<-resul[2];
+        registrarCandidato(candidatos,votos);
     }
     if(opt==3){
         listarCandidatos(candidatos);
     }
+    if(opt==4){
+        votar(candidatos,votos);
+    }
     
     if(opt==-1){
+        print(votos);
         return (0)
     }
+    
+    plot(votos,col="darkolivegreen1");
+    Sys.sleep(0)
 }
